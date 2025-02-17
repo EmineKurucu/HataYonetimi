8ed248e4994f8c821056b0748524a1e9bea8ac73
Emine <emine.kurucu.7@gmail.com>
2025-02-17 12:06:19 +0300

    error handling giris

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,22 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        try{
+            int[] sayilar = new int[]{1,2,3};
+            System.out.println(sayilar[3]); // 3. index yok
+        } catch (Exception exception){
+            System.out.println("Hata!");
+            System.out.println(exception);
+        }finally {
+            System.out.println("Ne olursa olsun burasi calisir");
         }
+
+        // ornegin veritabanına bağlandın ve hata oluştu veri tabanına bağlantı oluşturduktan sonra
+        // çıkarken bağlantıyı kapatman gerekir finally bloğunu böyle bir durumda kullan
+        // birden fazla catch bloğu yazılabilir
+
+        /* exception yazılan koddan alınan hatalardır. Try catch ile kontrol altına alınabilir
+        Errorlar kodlarla müdahale edemeyeceğimiz hatalardır
+         */
     }
 }
